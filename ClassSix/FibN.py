300911e70048eced007ec0c56808eb500b2773e9
linwang <w_lin1025@163.com>
2017-07-16 07:47:34 +0800

    Fid

---
+++ ClassSix/FibN.py
@@ -0,0 +1,12 @@
+def FibN(num):
+  result = [0,1]
+  while(num<=0):
+    num = input('How Many Fib Do You Want Get:')
+  if num == 1:
+    result = [0]
+  for i in range(num-2):
+    result.append(result[-2]+result[-1])
+  return result
+
+num = input('How Many Fib Do You Want Get:')
+print FibN(num)
