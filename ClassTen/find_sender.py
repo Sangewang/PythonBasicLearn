adfc7c7d991650a8d48396396a99067b6fea9856
linwang <w_lin1025@163.com>
2017-08-02 08:30:01 +0800

    add

--- ClassTen/find_sender.py
+++ ClassTen/find_sender.py
@@ -3,16 +3,17 @@ pat = re.compile('From: (.*) <.*?>$')
 for line in fileinput.input():
   m = pat.match(line)
   if m:
-    print m.group(0)
-    print m.group(1)
+    print 'Find The Target Line: ',m.group(0)
+    print 'Find The Sender: ',m.group(1)
 
-
+print '\n'
 pat = re.compile(r'[a-z\-\.]+@[a-z\-\.]+',re.IGNORECASE)
 addresses =  set()
 for line in fileinput.input():
   for address in pat.findall(line):
     addresses.add(address)
 
+print 'Find The Email All Address: '
 for address in sorted(list(addresses)):
   print address
 
