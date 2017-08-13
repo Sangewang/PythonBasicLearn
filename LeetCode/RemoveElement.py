f3b9cf0ed8e52b7ccca0e35f8cf4b53058795193
linwang <w_lin1025@163.com>
2017-08-13 22:52:09 +0800

    RemoveElement

--- LeetCode/RemoveElement.py
+++ LeetCode/RemoveElement.py
@@ -3,17 +3,33 @@ class Demo:
     if not nums:
       return 0
     cnt = len(nums)
-    flag = -1
+    flag_j = -1
     match = 0
+
     for i in range(cnt):
       if nums[i] == value:
-        flag = i
+        if i > flag_j:
+          j = i+1
+        else:
+          j = flag_j+1
+        while(j<cnt):
+          if nums[j]!=nums[i]:
+            nums[i] = nums[j]
+            flag_j = j
+          j+=1
+    for i in range(cnt):
+      if nums[i] != value:
         match+=1
       else:
-        if flag != -1:
-          nums[flag] = nums[i]
-    result = match
-    return result,nums
+        break
+    return match,nums
+
+
 
 Test = Demo()
 print Test.RemoveElement([1,2],1)
+print Test.RemoveElement([3,2,2,3],3)
+print Test.RemoveElement([1,1],1)
+print Test.RemoveElement([],1)
+print Test.RemoveElement([1],1)
+print Test.RemoveElement([2,2,3],2)
