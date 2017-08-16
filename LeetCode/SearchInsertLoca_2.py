8ddcdc98a649c2a29d626bb3f7429e946c26c805
linwang <w_lin1025@163.com>
2017-08-16 22:11:58 +0800

    er fen

--- LeetCode/SearchInsertLoca_2.py
+++ LeetCode/SearchInsertLoca_2.py
@@ -3,13 +3,13 @@ class Solution(object):
       cnt = len(nums)
       start = 0
       end   = cnt-1
-      while(start < end):
-        mid = (end + start)//2
+      while(start <= end):
+        mid = start + (end-start)//2
         if nums[mid] == target:
           return mid
         else:
           if nums[mid] > target:
-            end = mid
+            end = mid - 1
           else:
             start = mid + 1
       return start
@@ -18,3 +18,4 @@ Test = Solution()
 nums = [1,3,5,6]
 print(Test.searchInsert(nums,5))
 print(Test.searchInsert(nums,2))
+print(Test.searchInsert(nums,7))
