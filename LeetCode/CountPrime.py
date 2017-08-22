10135e1305462025d2d679195bb0466328cd17d5
linwang <w_lin1025@163.com>
2017-08-23 06:46:10 +0800

    add Count Prime

--- LeetCode/CountPrime.py
+++ LeetCode/CountPrime.py
@@ -2,20 +2,22 @@ class Solution(object):
   def countPrimes(self, n):
     count = 0
     result = []
+    output = []
     if n<2:
       return count
-    
+    result = list(range(0,n))
+    upper = int(pow(n,0.5))
     for i in range(2,n):
-      upper = int(pow(i,0.5)) + 1
-      
-      flag = 0
-      for j in range(2,upper):
-        if i%j == 0:
-          flag = 1
-          break
-      if(flag == 0 ):
+      if result[i]:
+        j = i * i
+        while(j < n):
+          result[j] = 0
+          j+=i
+    for i in range(2,n):
+      if result[i] != 0:
         count+=1
-        result.append(i)
-    return count,result
+        output.append(result[i])
+    return count
+
 Test = Solution()
 print(Test.countPrimes(100000))
