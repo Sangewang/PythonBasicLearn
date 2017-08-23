90d2632be193a9fe7970f0784a09f4d4c54a1708
linwang <w_lin1025@163.com>
2017-08-23 20:55:05 +0800

    add slice list

--- test.py
+++ test.py
@@ -1,6 +1,26 @@
-def func(*arg_2,**arg_3):
-  print(type(arg_2),arg_2)
-  print(type(arg_3),arg_3)
+li = list(range(10))
+print('li=',li)
 
-func('Tom',1,2,3,china = 'Beijing',uk = 'London')
-func('Tom',1,3)
+print('li[2:8]=',li[2:8])
+print('li[::-1]',li[::-1])
+
+
+
+
+
+li = [i*2 for i in range(10)]
+print('li = ',li)
+
+li_2d = [ [0] * 3] *3
+li_2d = [ [0] * 3  for i in range(3)]
+print('li_2d = ',li_2d)
+print('li_2d[0][0]=',li_2d[0][0])
+li_2d[0][0] = 100
+print('li_2d[0][0]=',li_2d[0][0])
+print('li_2d = ',li_2d)
+
+s = {x for x in range(10) if x % 2==0}
+print(s)
+
+d = {x:x%2==0 for x in range(10)}
+print(d)
