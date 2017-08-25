499131c1aae7b955a47558fbc602c6672775ed6b
linwang <w_lin1025@163.com>
2017-08-25 20:56:45 +0800

    add mergeSort

---
+++ LeetCode/MergeSortArray.py
@@ -0,0 +1,12 @@
+class Solution(object):
+  def merge(self, nums1, m, nums2, n):
+    print('nums1 = ',nums1)
+    nums1.extend(nums2) 
+    print('nums1 = ',nums1)
+    nums1.sort()
+    print('nums1 = ',nums1)
+    return nums1
+Test = Solution()
+nums1 = [10,8,6,4,2]
+nums2 = [9,7,5,3,1]
+print(Test.merge(nums1,5,nums2,5))
